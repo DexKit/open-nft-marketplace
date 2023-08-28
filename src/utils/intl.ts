717e2f977b3966ba74e029429ec71863f36e67be
NFTcka <123768408+NFTcka@users.noreply.github.com>
2023-08-28 11:00:19 +0200

    feat(lang): add czech language (#16)
    feat: add Czech language

--- src/utils/intl.ts
+++ src/utils/intl.ts
@@ -1,6 +1,7 @@
 import enUs from '../../compiled-lang/en-US.json';
 import ptBR from '../../compiled-lang/pt-BR.json';
 import esES from '../../compiled-lang/es-ES.json';
+import csCZ from '../../compiled-lang/cs-CZ.json';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -8,6 +9,7 @@ const COMPILED_LANGS: { [key: string]: any } = {
   'en-US': isProduction ? enUs : enUs,
   'pt-BR': isProduction ? ptBR : ptBR,
   'es-ES': isProduction ? esES : esES,
+  'cs-CZ': isProduction ? csCZ : csCZ,
 };
 
 export function loadLocaleData(locale: string) {
